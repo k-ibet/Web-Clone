7972f68b91ccc51f8e6fa632568a0bae40f1651a
k-ibet <chirchir12@outlook.com>
2021-07-09 23:23:13 +0300

    <master css tutorial>

---
+++ app.js
@@ -0,0 +1,18 @@
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+const solarButton = document.getElementById('solar');
+const body = document.body;
+
+//Button Event Handlers
+
+darkButton.onclick = () => {
+    body.classList.replace('light', 'dark');
+};
+
+lightButton.onclick = () => {
+    body.classList.replace('dark', 'light');
+}
+
+solarButton.onclick = () => {
+
+};
